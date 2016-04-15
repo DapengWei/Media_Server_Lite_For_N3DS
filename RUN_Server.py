ea09d5c6089cb4ecc46c4764277490b88eee8a1a
Dapeng Wei <dapeng.wei@acadine.com>
2016-04-15 16:20:57 +0800

    First Version
    This is the first version.

---
+++ RUN_Server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def list_movies():
+    movies_list = []
+    for dirpath, dirnames, filenames in os.walk("./static/movie"):
+            for filename in filenames:
+                if os.path.splitext(filename)[-1] == ".mp4":
+                    movies_list.append(filename)
+    return render_template('homepage.html', movies_list=movies_list)
+
+
+@app.route('/player/<movie_name>')
+def play_movie(movie_name):
+    return render_template('player.html', movie_name=movie_name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
